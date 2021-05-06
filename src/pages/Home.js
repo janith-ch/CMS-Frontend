cd2cf25ad9a11fab2fcf09cf1546c61e213bf1f1
Ranjula Wijesinghe <ranjula29@gmail.com>
2021-05-06 21:34:30 +0530

    Create Admin sidebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,13 @@ import React from "react";
 import "../app/App.css";
 import Footer from "../components/home/Footer";
 import HeroSection from "../components/home/HeroSection";
+import Navbar from "../components/home/Navbar";
 
 function Home() {
   return (
     <>
+       <Navbar/>
       <HeroSection />
-      {/*<Description1/>*/}
-      {/*<ProductList/>*/}
-      {/*<History/>*/}
-      {/*<Cards/>*/}
-
       <Footer />
     </>
   );
