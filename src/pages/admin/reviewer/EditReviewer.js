37983149aa7e8327e9990bf6b10bdb371412c2f4
gagana <gagana@codevus.com>
2021-05-27 19:21:53 +0530

    Reviewer

--- src/pages/admin/reviewer/EditReviewer.js
+++ src/pages/admin/reviewer/EditReviewer.js
@@ -1,11 +1,189 @@
 /** @format */
 
 import React, { Component } from "react";
+import { Col, Button, Form, Row } from "react-bootstrap";
+import { getSingleUser, updateUser } from "../../../service/User";
 
 class EditReviewer extends Component {
-  state = {};
+  state = {
+    id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    userRole: "",
+    password: "",
+    country: "",
+  };
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    try {
+      const response = await getSingleUser(this.props.match.params.id);
+
+      this.setState({
+        id: response.data.id,
+        firstName: response.data.firstName,
+        lastName: response.data.lastName,
+        email: response.data.email,
+        userRole: response.data.userRole,
+        password: response.data.password,
+        country: response.data.country,
+      });
+    } catch (e) {
+      // error handling
+      console.log(e);
+    }
+  };
+  onChangeFirstName = (e) => {
+    this.setState({
+      firstName: e.target.value,
+    });
+  };
+  onChangeLastName = (e) => {
+    this.setState({
+      lastName: e.target.value,
+    });
+  };
+  onChangeEmail = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  onChangePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+  onChangeCountry = (e) => {
+    this.setState({
+      country: e.target.value,
+    });
+  };
+  onChnageUserRole = (e) => {
+    this.setState({
+      userRole: e.target.value,
+    });
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      id: this.state.id,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      userRole: this.state.userRole,
+      password: this.state.password,
+      country: this.state.country,
+    };
+    try {
+      const response = await updateUser(this.props.match.params.id, user);
+      // success scenario handle here
+      if (response.data) {
+        console.log(response.data);
+      }
+    } catch (ex) {
+      // error handling
+      console.log(e);
+    }
+
+    this.setState({ firstName: "" });
+    this.setState({ lastName: "" });
+    this.setState({ email: "" });
+    this.setState({ userRole: "" });
+    this.setState({ password: "" });
+    this.setState({ country: "" });
+  };
+
   render() {
-    return <div>edit work</div>;
+    return (
+      <Row>
+        <Col md="2"></Col>
+        <Col md="8">
+          <br></br>
+          <center>
+            <b>EDIT REVIEWER</b>
+          </center>
+          <Form onSubmit={this.onSubmit}>
+            <Form.Group>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="firstName"
+                placeholder="Enter first name"
+                value={this.state.firstName}
+                onChange={this.onChangeFirstName}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="lastName"
+                placeholder="Enter last name"
+                value={this.state.lastName}
+                onChange={this.onChangeLastName}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={this.state.email}
+                onChange={this.onChangeEmail}
+              />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>User Role</Form.Label>
+              <Form.Control
+                as="select"
+                value={this.state.userRole}
+                onChange={this.onChnageUserRole}
+              >
+                <option>user</option>
+                <option>ws_user</option>
+                <option>researcher</option>
+                <option>presenter</option>
+                <option>attendee</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.onChangePassword}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Country</Form.Label>
+              <Form.Control
+                type="country"
+                placeholder="Enter country"
+                value={this.state.country}
+                onChange={this.onChangeCountry}
+              />
+            </Form.Group>
+
+            <Form.Group id="formGridCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+
+            <Button variant="success" type="submit">
+              Update Revieweer
+            </Button>
+            <br></br>
+            <br></br>
+          </Form>
+        </Col>
+        <Col md="2"></Col>
+      </Row>
+    );
   }
 }
 
--- src/pages/admin/reviewer/ReviewerList.js
+++ src/pages/admin/reviewer/ReviewerList.js
@@ -1,10 +1,159 @@
 /** @format */
 
 import React, { Component } from "react";
+import Swal from "sweetalert2";
+import "../../../../node_modules/datatables.net-dt/css/jquery.dataTables.css";
+import { Col, Button, Row } from "react-bootstrap";
+import { getUserList, deleteUser } from "../../../service/User";
+import UserBody from "../../../components/admin/user/UserBody";
+import { USERS } from "../UserRole";
+const $ = require("jquery");
+$.DataTable = require("datatables.net");
+
 class ReviewerList extends Component {
-  state = {};
+  state = { users: [] };
+
+  componentDidMount() {
+    this.fetchUser();
+  }
+
+  fetchUser = async () => {
+    try {
+      const response = await getUserList(`${USERS}`);
+      console.log(response.data);
+      this.setState({ users: response.data || [] });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  removeUser = async (id) => {
+    const response = await deleteUser(id);
+    console.log(response.data);
+    try {
+      this.setState({
+        users: this.state.users.filter((el) => el.id !== id),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  userList() {
+    return this.state.users.map((currentUser) => {
+      return (
+        <UserBody
+          user={currentUser}
+          deleteAlert={this.deleteAlert}
+          editUser={this.editUser}
+          key={currentUser.id}
+        />
+      );
+    });
+  }
+  addUser() {
+    this.props.history.push("/admin/user-list/create");
+  }
+  editUser = (id) => {
+    this.props.history.push("/admin/user-list/edit/" + id);
+  };
+
+  deleteAlert = (id) => {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success m-2",
+        cancelButton: "btn btn-danger m-2",
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: "Are you sure?",
+        text: "Do you want to remove this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.removeUser(id);
+          swalWithBootstrapButtons.fire(
+            "Deleted!",
+            "User has been removed.",
+            "success"
+          );
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            "Cancelled",
+            "User not removed :)",
+            "error"
+          );
+        }
+      });
+  };
+
+  componentDidUpdate() {
+    this.$el = $(this.el);
+    this.$el.DataTable();
+  }
+
   render() {
-    return <div>r work</div>;
+    return (
+      <div>
+        <Row>
+          <Col md="4"></Col>
+          <Col md="6">
+            <h3>
+              <b>Users List</b>
+            </h3>
+          </Col>
+          <Col md="2">
+            <Button
+              variant="primary"
+              onClick={() => {
+                this.addUser();
+              }}
+            >
+              <b>Add User</b>
+            </Button>
+          </Col>
+        </Row>
+        <br></br>
+
+        <table
+          className="display"
+          ref={(el) => (this.el = el)}
+          style={{ boxShadow: "8px 8px  #dce3e0" }}
+        >
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Country</th>
+              <th>User Type</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>{this.userList()}</tbody>
+          <tfoot>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Country</th>
+              <th>User Type</th>
+              <th>Action</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
   }
 }
 
